d12e3a54dc293c48555439d214363270cfbb334c
wly <1355498705@qq.com>
2019-04-22 20:21:22 +0800

    歌手列表完成

--- src/components/base/SingerList.vue
+++ src/components/base/SingerList.vue
@@ -1,10 +1,11 @@
 <template>
     <ul class="singerList_wrap">
         <li v-for="item in singerList" :key="item.id">
-            <h1 class="title_name">{{item.name}}</h1>
+            <h1 class="title_name">{{item.title}}</h1>
             <ul class="singerList_item">
-                <li v-for="i in item" :key="i.id">
-                    <img ="" alt="">
+                <li v-for="i in item.item" :key="i.id" class="item_wrap">
+                    <img v-lazy="i.avatar" alt="">
+                    <span class="text">{{i.name}}</span>
                 </li>
             </ul>
         </li>
@@ -21,10 +22,39 @@
           return []
         }
       }
+    },
+    mounted() {
+      console.log(this);
     }
   }
 </script>
 
+
 <style scoped lang="stylus" rel="stylesheet/stylus">
+    @import '../../common/stylus/variable.styl'
+.singerList_wrap
+    color: $color-text
+
+    .title_name
+        text-align left
+        padding 8px 7px
+        font-size $font-size-medium-x
+        background $color-dialog-background
+    .singerList_item
+        font-size $font-size-medium
+        .item_wrap
+            display flex
+            padding 20px 0 0 30px
+            img
+                width 50px
+                height:50px
+                border-radius 50%
+            .text
+                display flex
+                align-items center
+                padding-left 10px
+        .item_wrap:last-child
+            padding-bottom: 30px;
+
 
 </style>
--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -1,83 +1,99 @@
 <template>
-  <div class="singer">歌手页面</div>
+    <div class="singer">
+        <BetterScroll :data="singerList">
+            <div>
+                <SingerList :singerList="singerList"></SingerList>
+            </div>
+        </BetterScroll>
+    </div>
 </template>
 
 <script>
-    import {getSingerList} from '../api/singer'
-    import {ERR_OK} from '../api/config'
-    import Singer from '../common/js/singer'
-    const HOT_NAME='热门'
-    const SHOW_LENGTH=10
-export default {
-  name: 'Singer',
-  data(){
-    return {
-      singerList:[]
-    }
-  },
-  mounted() {
-    this._getSingerList()
-  },
-  methods:{
-    //获取歌手列表
-    _getSingerList(){
-      getSingerList().then((res)=>{
-        if(res.code===ERR_OK){
-          this.singerList=this._normalSinger(res.data.list)
-          console.log(this.singerList);
-        }
-      })
-    },
-    //格式化数据
-    _normalSinger(list){
-      let map={
-        hot:{
-          title:HOT_NAME,
-          item:[]
-        }
+  import {getSingerList} from '../api/singer'
+  import SingerList from '../components/base/SingerList'
+  import BetterScroll from '../components/base/BetterScroll'
+  import {ERR_OK} from '../api/config'
+  import Singer from '../common/js/singer'
+
+  const HOT_NAME = '热门'
+  const SHOW_LENGTH = 10
+  export default {
+    name: 'Singer',
+    data() {
+      return {
+        singerList: []
       }
-      //对数据按字母进行分类
-      list.forEach((item,index)=>{
-        if(index<SHOW_LENGTH){
-          //console.log(item);
-          map.hot.item.push(new Singer({
-            id:item.Fsinger_mid,
-            name:item.Fsinger_name
-          }))
-        }
-        let key =item.Findex
-        if(!map[key]){
-          map[key]={
-            title:key,
-            item:[]
+    },
+    components: {SingerList, BetterScroll},
+    mounted() {
+      this._getSingerList()
+    },
+    methods: {
+      //获取歌手列表
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singerList = this._normalSinger(res.data.list)
+            //console.log(this.singerList);
+          }
+        })
+      },
+      //格式化数据
+      _normalSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            item: []
           }
         }
-        map[key].item.push(new Singer({
-          id:item.Fsinger_mid,
-          name:item.Fsinger_name
-        }))
-      })
-      //console.log(map)
-      //获取有序的数据列表
-      let hot=[]
-      let ret=[]
-      for(let key in map){
-        let val=map[key]
-        if(val.title.match(/[a-zA-Z]/)){
-          ret.push(val)
-        }else{
-          hot.push(val)
+        //对数据按字母进行分类
+        list.forEach((item, index) => {
+          if (index < SHOW_LENGTH) {
+            //console.log(item);
+            map.hot.item.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          let key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              item: []
+            }
+          }
+          map[key].item.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        //console.log(map)
+        //获取有序的数据列表
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title===HOT_NAME) {
+            hot.push(val)
+          }
         }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
-      ret.sort((a,b)=>{
-        return a.title.charCodeAt(0)-b.title.charCodeAt(0)
-      })
-      return hot.concat(ret)
     }
   }
-}
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+@import '../common/stylus/variable.styl';
+.singer
+    position fixed
+    top 88px
+    bottom 0
+    z-index:-1
+    width 100%
 </style>
