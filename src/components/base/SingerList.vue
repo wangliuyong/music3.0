e4205b7ac67b7d7fcc71f1cfa883b94407b679b2
wly <1355498705@qq.com>
2019-04-22 19:44:46 +0800

    获取歌手列表并进行处理

---
+++ src/components/base/SingerList.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul class="singerList_wrap">
+        <li v-for="item in singerList" :key="item.id">
+            <h1 class="title_name">{{item.name}}</h1>
+            <ul class="singerList_item">
+                <li v-for="i in item" :key="i.id">
+                    <img ="" alt="">
+                </li>
+            </ul>
+        </li>
+    </ul>
+</template>
+
+<script>
+  export default {
+    name: "SingerList",
+    props:{
+      singerList:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -3,8 +3,78 @@
 </template>
 
 <script>
+    import {getSingerList} from '../api/singer'
+    import {ERR_OK} from '../api/config'
+    import Singer from '../common/js/singer'
+    const HOT_NAME='热门'
+    const SHOW_LENGTH=10
 export default {
-  name: 'Singer'
+  name: 'Singer',
+  data(){
+    return {
+      singerList:[]
+    }
+  },
+  mounted() {
+    this._getSingerList()
+  },
+  methods:{
+    //获取歌手列表
+    _getSingerList(){
+      getSingerList().then((res)=>{
+        if(res.code===ERR_OK){
+          this.singerList=this._normalSinger(res.data.list)
+          console.log(this.singerList);
+        }
+      })
+    },
+    //格式化数据
+    _normalSinger(list){
+      let map={
+        hot:{
+          title:HOT_NAME,
+          item:[]
+        }
+      }
+      //对数据按字母进行分类
+      list.forEach((item,index)=>{
+        if(index<SHOW_LENGTH){
+          //console.log(item);
+          map.hot.item.push(new Singer({
+            id:item.Fsinger_mid,
+            name:item.Fsinger_name
+          }))
+        }
+        let key =item.Findex
+        if(!map[key]){
+          map[key]={
+            title:key,
+            item:[]
+          }
+        }
+        map[key].item.push(new Singer({
+          id:item.Fsinger_mid,
+          name:item.Fsinger_name
+        }))
+      })
+      //console.log(map)
+      //获取有序的数据列表
+      let hot=[]
+      let ret=[]
+      for(let key in map){
+        let val=map[key]
+        if(val.title.match(/[a-zA-Z]/)){
+          ret.push(val)
+        }else{
+          hot.push(val)
+        }
+      }
+      ret.sort((a,b)=>{
+        return a.title.charCodeAt(0)-b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
 }
 </script>
 
